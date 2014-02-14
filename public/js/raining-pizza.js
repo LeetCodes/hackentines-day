67bad2684730c16bab0f04868745bca212247577
Alex Forsythe <alexwforsythe@gmail.com>
2014-02-14 17:06:44 -0600

    improved raining-pizza functions and added binary card-text

--- public/js/raining-pizza.js
+++ public/js/raining-pizza.js
@@ -1,39 +1,52 @@
-sliceCount = 0;
+var sliceCount = 30;
 
 function makeItRain() {
-    if (sliceCount > 10)
-        return false
-    else {
-        var randomTime = Math.floor(Math.random() * (500) * 2);
-        setTimeout(function () {
-            sliceCount++;
-            rainPizza();
-            makeItRain();
-        }, randomTime);
-    }
-}
-
-function rainPizza() {
-    var slice = $('<img class="slice" alt="*" src="images/pizza-slice-icon-1.png">');
-    $('#raining-pizza').prepend(slice);
-
     var w1 = $(window).width();
     var w2 = $('#raining-pizza').width();
-    sliceX = ((w1 - w2) / 2) + (Math.floor(Math.random() * w2)) - 20;
-    sliceSpd = Math.floor(Math.random() * (500) * 20);
+    var w3 = (w1 - w2) / 2;
 
-    slice.css({
-        'left': sliceX + 'px',
-        'top': '65px'
-    });
+    var $slices = $(),
+        createSlices = function () {
+            for (var i = 0; i < sliceCount; ++i) {
+                var $slice = $('<img class="slice" alt="*" src="images/pizza-slice-icon-1.png">');
+                var sliceX = w3 + Math.random() * w2 * 0.9;
 
-    slice.animate({
-        top: "550px",
-        opacity: "0",
-    }, 2000, function () {
-        $(this).remove();
+                $slice.css({
+                    'left': sliceX + 'px',
+                    'top': '65px'
+                });
+
+                $slices = $slices.add($slice);
+            }
+
+            $('#raining-pizza').prepend($slices);
+        },
+
+        rainPizza = function() {
+            console.log('animating slices');
+            $slices.each(function() {
+                var singleAnimation = function($slice) {
+                    $slice.animate({
+                        top: "550px",
+                        opacity: "0",
+                    }, Math.random() * -2500 + 5000, function() {
+                        var sliceX = w3 + Math.random() * w2 * 0.9;
+                        $slice.css({
+                            'left': sliceX + 'px',
+                            'top': '65px',
+                            'opacity': 1
+                        });
+
+                        singleAnimation($slice);
+                    });
+                };
+
+                singleAnimation($(this));
+            });
+        };
+
+        createSlices();
         rainPizza();
-    });
 }
 
 // makeItRain();
